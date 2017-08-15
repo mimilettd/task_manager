6f5a13b1e60f07c7e8027f77125edb0da4c1d18b
mimilettd <mimilettd@gmail.com>
2017-08-14 20:51:08 -0600

    edit, update, and delete tasks

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -4,6 +4,7 @@ require_relative '../models/task.rb'
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get '/' do
     erb :dashboard
@@ -35,6 +36,21 @@ class TaskManagerApp < Sinatra::Base
   end
 
   get '/showmethetask' do
+    redirect '/tasks'
+  end
+
+  get '/tasks/:id/edit' do
+    @task = Task.new(params[:id])
+    erb :edit
+  end
+
+  put '/tasks/:id' do |id|
+    Task.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
+  end
+
+  delete '/tasks/:id' do |id|
+    Task.destroy(id.to_i)
     redirect '/tasks'
   end
 end
